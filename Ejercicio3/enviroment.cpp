8eccbdf317cd125529a47793145e0625027eedab
ddealsan <ddealsan@myuax.com>
2023-12-05 12:06:10 +0100

    Realizamos el codigo ajustandonos a lo que nos pide

--- Ejercicio3/enviroment.cpp
+++ Ejercicio3/enviroment.cpp
@@ -1,5 +1,27 @@
-//
-// Created by dalfo on 05/12/2023.
-//
+#include "Enviroment.h"
 
-#include "enviroment.h"
+
+void Enviroment::insert(const std::string& name, const Variant& value) {
+    symbolTable[name] = value;
+}
+
+
+Variant Enviroment::getValue(const std::string& name) const {
+    auto it = symbolTable.find(name);
+    if (it != symbolTable.end()) {
+        return it->second;
+    }
+
+    return Variant(); // Devuelve un Variant por defecto
+}
+
+
+bool Enviroment::lookup(const std::string& name, Variant& value) const {
+    auto it = symbolTable.find(name);
+    if (it != symbolTable.end()) {
+        value = it->second;
+        return true;
+    }
+
+    return false;
+}
