03fc5487c0a29344e9eb1f568a05aac02c4d2304
ddealsan <ddealsan@myuax.com>
2023-12-05 11:18:18 +0100

    rellenamos enviroment.cpp

--- Ejercicio1/enviroment.cpp
+++ Ejercicio1/enviroment.cpp
@@ -1,2 +1,16 @@
 #include "environment.h"
 #include <iostream>
+
+void Environment::setVariable(const std::string& name, const Variant& value) {
+    symbolTable[name] = value;
+}
+
+Variant Environment::getVariable(const std::string& name) const {
+    auto it = symbolTable.find(name);
+    if (it != symbolTable.end()) {
+        return it->second;
+    } else {
+        std::cerr << "Error: Variable '" << name << "' no encontrada en el entorno.\n";
+        return Variant();
+    }
+}
