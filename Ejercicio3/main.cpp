f71f59729dce553b18886269c82994fc4250f9c2
ddealsan <ddealsan@myuax.com>
2023-12-05 12:07:11 +0100

    Relizamos el main y corregimos errores

--- Ejercicio3/main.cpp
+++ Ejercicio3/main.cpp
@@ -1,3 +1,34 @@
-//
-// Created by dalfo on 05/12/2023.
-//
+#include "Enviroment.h"
+
+int main() {
+
+    Enviroment enviroment;
+
+
+    Variant health(100);
+    Variant position(std::make_pair(10.0, 20.0));
+
+
+    enviroment.insert("playerHealth", health);
+    enviroment.insert("playerPosition", position);
+
+
+    Variant retrievedHealth;
+    Variant retrievedPosition;
+
+    if (enviroment.lookup("playerHealth", retrievedHealth)) {
+        std::cout << "Player Health: " << std::get<int>(retrievedHealth.getValue()) << std::endl;
+    } else {
+        std::cout << "Player Health not found in the environment." << std::endl;
+    }
+
+    if (enviroment.lookup("playerSpeed", retrievedPosition)) {
+        auto pos = std::get<std::pair<double, double>>(retrievedPosition.getValue());
+        std::cout << "Player Position: (" << pos.first << ", " << pos.second << ")" << std::endl;
+    } else {
+        std::cout << "Player Position not found in the environment." << std::endl;
+    }
+
+
+    return 0;
+}
