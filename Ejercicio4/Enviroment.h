11ec91dbb5c6e9d61633fe6d627dc9b0ea09308a
ddealsan <ddealsan@myuax.com>
2023-12-05 12:14:49 +0100

    añadimos comentarios y completamos codigo

--- Ejercicio4/Enviroment.h
+++ Ejercicio4/Enviroment.h
@@ -13,17 +13,20 @@ private:
     std::map<std::string, Variant> symbolTable;
 
 public:
+    // Estructura básica
     Enviroment() = default; // Constructor por defecto
 
+    // Método para insertar un nuevo símbolo en el entorno
     void insert(const std::string& name, const Variant& value);
 
+    // Método para obtener el valor de un símbolo
     Variant getValue(const std::string& name) const;
 
-
+    // Método para buscar un símbolo en el entorno
     bool lookup(const std::string& name, Variant& value) const;
 
+    // Otros métodos y funciones que puedas necesitar en la clase Enviroment
 };
-
 #endif //EXAMENTEMA3_ENVIROMENT_H
 
 #endif //EXAMENTEMA3_ENVIROMENT_H
--- Ejercicio4/main.cpp
+++ Ejercicio4/main.cpp
@@ -1,3 +1,31 @@
-//
-// Created by dalfo on 05/12/2023.
-//
+#include "Enviroment.h"
+
+int main() {
+    try {
+        // Crear una instancia de Enviroment
+        Enviroment enviroment;
+
+        // ... (resto del código)
+
+        // Ejemplos de uso con manejo de excepciones
+        try {
+            Variant retrievedHealth = enviroment.getValue("playerHealth");
+            std::cout << "Player Health: " << std::get<int>(retrievedHealth.getValue()) << std::endl;
+        } catch (const std::runtime_error& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+
+        try {
+            enviroment.insert("playerHealth", Variant(150)); // Intentar insertar un símbolo existente
+        } catch (const std::runtime_error& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+
+        // ... (resto del código)
+
+    } catch (const std::exception& e) {
+        std::cerr << "Excepción general: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
