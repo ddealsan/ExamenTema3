4100010f00068107246837538a9bca9af29f3ce3
ddealsan <ddealsan@myuax.com>
2023-12-05 11:58:55 +0100

    Cambiamos errores

--- Ejercicio2/main.cpp
+++ Ejercicio2/main.cpp
@@ -16,6 +16,13 @@ int main() {
     Variant retrievedHealth = enviroment.getValue("playerHealth");
     Variant retrievedPosition = enviroment.getValue("playerPosition");
 
+    // Mostrar los valores obtenidos
+    std::cout << "Player Health: " << std::get<int>(retrievedHealth.getValue()) << std::endl;
+
+    auto pos = std::get<std::pair<double, double>>(retrievedPosition.getValue());
+    std::cout << "Player Position: (" << pos.first << ", " << pos.second << ")" << std::endl;
+
+    // Realizar otras operaciones según sea necesario
 
     return 0;
 }
