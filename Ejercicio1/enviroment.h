c280d2f526fb8f2e98476c08e46cca37e098ffc9
ddealsan <ddealsan@myuax.com>
2023-12-05 11:14:40 +0100

    Creamos un cpp y un .h

---
+++ Ejercicio1/enviroment.h
@@ -0,0 +1,14 @@
+//
+// Created by dalfo on 05/12/2023.
+//
+
+#ifndef EXAMENTEMA3_ENVIROMENT_H
+#define EXAMENTEMA3_ENVIROMENT_H
+
+
+class enviroment {
+
+};
+
+
+#endif //EXAMENTEMA3_ENVIROMENT_H
