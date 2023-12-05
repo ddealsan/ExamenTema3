9bea22a09941adab8bf4f5118ccdf0f37200f7b3
ddealsan <ddealsan@myuax.com>
2023-12-05 12:10:06 +0100

    Rellenamos el variant.h

--- Ejercicio4/variant.h
+++ Ejercicio4/variant.h
@@ -1,8 +1,28 @@
-//
-// Created by dalfo on 05/12/2023.
-//
-
 #ifndef EXAMENTEMA3_VARIANT_H
 #define EXAMENTEMA3_VARIANT_H
+
+#include <variant>
+#include <iostream>
+#include <map>
+#include <string>
+
+class Variant {
+public:
+    std::variant<int, std::pair<double, double>> data;
+
+    // Constructor por defecto
+    Variant() : data() {}
+
+    // Constructor para enteros
+    Variant(int value) : data(value) {}
+
+    // Constructor para pares de double
+    Variant(const std::pair<double, double>& value) : data(value) {}
+
+    // Obtener el valor
+    std::variant<int, std::pair<double, double>> getValue() const {
+        return data;
+    }
+};
 
 #endif //EXAMENTEMA3_VARIANT_H
